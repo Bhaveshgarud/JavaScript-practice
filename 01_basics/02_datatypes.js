3cabb08004d371e1660d2e7fe53fde7bbf0e4123
Bhavesh Garud <bharatchandra949@gmail.com>
2024-12-16 17:25:14 +0530

    add 02_datatypes file and it's learnings

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,13 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3); // we are using nodejs not browser so cannot be run because the javascript engine is inside the browser
+
+// number
+// bigint
+// string
+// boolean
+// null
+// undefined
+// symbol
+
+// object
